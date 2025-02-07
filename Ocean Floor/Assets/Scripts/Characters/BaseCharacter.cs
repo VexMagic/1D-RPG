f6cbced23820218de5942eca6d19846477ae1fd4
VexMagic <89905295+VexMagic@users.noreply.github.com>
2025-02-07 13:20:48 +0100

    basic character script

---
+++ Ocean Floor/Assets/Scripts/Characters/BaseCharacter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacter : MonoBehaviour
+{
+    //Basic stats
+    [SerializeField] private int health;
+    [SerializeField] private int speed; //determines turn order
+    [SerializeField] private int actions; //action points per turn
+    [SerializeField] private float protection; //damage reduction (0f - 1f = 0% - 100%)
+    [SerializeField] private float crit; //critical hit chance (0f - 1f = 0% - 100%)
+    [SerializeField] private float dodge; //chance to take no damage (0f - 1f = 0% - 100%)
+}
